3e68bac3128a92b5e72697c31ca82d6315948cb1
ravali <ravali@example.com>
2023-03-08 15:18:35 +0000

    musicplayer

---
+++ src/main/java/com/example/song/SongRepository.java
@@ -0,0 +1,12 @@
+package com.example.song;
+import java.util.*;
+
+public interface SongRepository {
+    ArrayList<Song> getAllSongs();
+    Song getSongById(int SongId);
+
+    Song addSong(Song song);
+    Song updateSong(int songId, Song song);
+
+    void  deleteSong(int songId);
+}    
